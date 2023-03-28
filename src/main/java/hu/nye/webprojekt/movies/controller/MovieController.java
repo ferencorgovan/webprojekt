3b22cfdb5560fdd04a1f6db6c25a53c224512b10
ferencorgovan <orgovanferike@gmail.com>
2023-03-28 14:05:30 +0200

    Added notFound status code

--- src/main/java/hu/nye/webprojekt/movies/controller/MovieController.java
+++ src/main/java/hu/nye/webprojekt/movies/controller/MovieController.java
@@ -2,6 +2,7 @@ package hu.nye.webprojekt.movies.controller;
 
 import hu.nye.webprojekt.movies.dto.MovieDTO;
 import hu.nye.webprojekt.movies.service.MovieService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,6 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping(path = "/movies")
 public class MovieController {
 
     private final MovieService movieService;
@@ -19,13 +21,24 @@ public class MovieController {
         this.movieService = movieService;
     }
 
-    @RequestMapping(path = "/movies", method = RequestMethod.GET)
+    @RequestMapping(path = "/", method = RequestMethod.GET)
     public List<MovieDTO> findAll() {
         return movieService.findAll();
     }
 
-    @RequestMapping(path = "/movies/{id}", method = RequestMethod.GET)
-    public Optional<MovieDTO> findById(@PathVariable Long id) {
-        return movieService.findById(id);
+    @RequestMapping(path = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<MovieDTO> findById(@PathVariable Long id) {
+        Optional<MovieDTO> movieDTO = movieService.findById(id);
+
+        ResponseEntity<MovieDTO> response;
+
+        if (movieDTO.isPresent()) {
+            response = ResponseEntity.ok(movieDTO.get());
+            // response = ResponseEntity.status(200).body(movieDTO.get());
+        } else {
+            response = ResponseEntity.notFound().build();
+            // response = ResponseEntity.status(404).build();
+        }
+        return response;
     }
 }
